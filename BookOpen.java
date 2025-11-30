40e0459a28f1a002406525752ab5841d911fbf66
Renda <abioulir@gmail.com>
2025-11-30 17:42:21 -0500

    I added the clue to the bookOpen class.

--- BookOpen.java
+++ BookOpen.java
@@ -25,31 +25,31 @@ public class BookOpen extends World
         
         GreenfootImage courseTitle = new GreenfootImage(600, 100);
         courseTitle.setFont(font);
-        courseTitle.setColor(Color.WHITE);
+        courseTitle.setColor(Color.BLACK);
         courseTitle.drawString("RIDDLE:", 20, 70);
         getBackground().drawImage(courseTitle, 50, 30);
         
         GreenfootImage line1 = new GreenfootImage(600, 100);
         line1.setFont(font);
-        line1.setColor(Color.WHITE);
+        line1.setColor(Color.BLACK);
         line1.drawString("WHAT DIP", 20, 70);
         getBackground().drawImage(line1, 50, 90);
         
         GreenfootImage line2 = new GreenfootImage(600, 100);
         line2.setFont(font);
-        line2.setColor(Color.WHITE);
+        line2.setColor(Color.BLACK);
         line2.drawString("CAN YOU MAKE", 20, 70);
         getBackground().drawImage(line2, 50, 130);
         
         GreenfootImage line3 = new GreenfootImage(600, 100);
         line3.setFont(font);
-        line3.setColor(Color.WHITE);
+        line3.setColor(Color.BLACK);
         line3.drawString("MAKE USING", 20, 70);
         getBackground().drawImage(line3, 50, 170);
         
         GreenfootImage line4 = new GreenfootImage(600, 100);
         line4.setFont(font);
-        line4.setColor(Color.WHITE);
+        line4.setColor(Color.BLACK);
         line4.drawString("AVOCADOS?", 20, 70);
         getBackground().drawImage(line4, 50, 210);
     }
