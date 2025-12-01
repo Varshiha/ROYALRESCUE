69564a59b32de09b19628adaeffac7fb0d9de5d5
Renda <abioulir@gmail.com>
2025-12-01 14:17:30 -0500

    King's room with miniLibrary

--- KingRoom.java
+++ KingRoom.java
@@ -16,6 +16,7 @@ public class KingRoom extends World
     public KingRoom()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(547, 554, 1); 
+        addObject( new MiniLibrary(), 235, 250); 
     }
 }
