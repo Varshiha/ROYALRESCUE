b7024bb9c99a044b5e1fa76ac41510b8b9059679
Renda <abioulir@gmail.com>
2025-12-04 13:42:32 -0500

    DarkForest modifications

--- DarkForest.java
+++ DarkForest.java
@@ -8,14 +8,41 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class DarkForest extends World
 {
-
+    private boolean finishedSpawning = false;
     /**
      * Constructor for objects of class DarkForest.
      * 
      */ 
     public DarkForest()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(547, 554, 1); 
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.        
+        super(547, 554, 1);
+        addObject(new Knight(), 350, 350);
+        act();
+        
+    }
+    
+    public void act(){
+        if(finishedSpawning && getObjects(Troll.class).isEmpty()){
+            checkKnightExit();
+            showText("E\n" + "X\n" + "I\n" + "T", 535, getHeight()/2);
+        }
+        
+    }
+    
+    private void prepare(int knightX, int knightY){
+        addObject(new Knight(), knightX, knightY);
+        addObject(new Troll(), Greenfoot.getRandomNumber(500), Greenfoot.getRandomNumber(500));
+        addObject(new Troll(), Greenfoot.getRandomNumber(500), Greenfoot.getRandomNumber(500));
+    }
+    
+    private void checkKnightExit(){
+        Knight k = (Knight)getObjects(Knight.class).get(0);
+        if(k.getX() >=getWidth() - 5){
+            int newX = k.getX();
+            int newY = k.getY();
+            Greenfoot.setWorld(new Grass2(newX, newY));
+
+        }
     }
 }
