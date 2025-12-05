d4a90277f011b69284fd1d913025a2f3d9a36ee7
Renda <abioulir@gmail.com>
2025-12-04 23:53:48 -0500

    Trying to fix the darkForest class.

--- DarkForest.java
+++ DarkForest.java
@@ -10,39 +10,51 @@ public class DarkForest extends World
 {
     private boolean finishedSpawning = false;
     private Knight knight = new Knight();
+    private boolean isTroll = true;
     /**
      * Constructor for objects of class DarkForest.
      * 
      */ 
     public DarkForest()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.        
         super(547, 554, 1);
         addObject(new Knight(), 350, 350);
-        
-        
+        act();
+
+        addObject(new Troll(), Greenfoot.getRandomNumber(500), Greenfoot.getRandomNumber(500));
+        addObject(new Troll(), Greenfoot.getRandomNumber(500), Greenfoot.getRandomNumber(500));
+
     }
-    
+
     public void act(){
-        if(finishedSpawning && getObjects(Troll.class).isEmpty()){
-            checkKnightExit();
-            showText("E\n" + "X\n" + "I\n" + "T", 535, getHeight()/2);
+        finalBoss();
+        if(getObjects(FinalBoss.class).isEmpty()){
+            saveKing();
         }
-        
     }
-    
-    private void prepare(int knightX, int knightY){
-        addObject(new Knight(), knightX, knightY);
-        addObject(new Troll(), Greenfoot.getRandomNumber(500), Greenfoot.getRandomNumber(500));
-        addObject(new Troll(), Greenfoot.getRandomNumber(500), Greenfoot.getRandomNumber(500));
+
+    public boolean finalBoss(){
+        if(getObjects(Troll.class).isEmpty()){
+            isTroll = false;
+        }
+        return false;
     }
-    
+
+    public void saveKing(){
+
+        if(getObjects(FinalBoss.class).isEmpty()){
+            checkKnightExit();
+            showText("K\n" + "I\n" + "N\n" + "G", 535, getHeight()/2);
+        }
+
+    }
+
     private void checkKnightExit(){
         Knight k = (Knight)getObjects(Knight.class).get(0);
         if(k.getX() >=getWidth() - 5){
             int newX = k.getX();
             int newY = k.getY();
-            Greenfoot.setWorld(new Grass2(newX, newY));
+            Greenfoot.setWorld(new Cave());
 
         }
     }
